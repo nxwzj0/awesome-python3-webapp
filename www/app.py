171ed613295955050e1f2ebb7370882e88824322
nxwzj0 <nxwzj0@126.com>
2018-05-09 13:11:40 +0800

    orm

--- www/app.py
+++ www/app.py
@@ -1,8 +1,10 @@
-import logging; logging.basicConfig(level=logging.INFO)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+__author__ = 'PayneV'
+import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from datetime import datetime
-
 from aiohttp import web
 
 def index(request):
